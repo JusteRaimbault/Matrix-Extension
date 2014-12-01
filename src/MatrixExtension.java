fbf4a812a7dc5dccacc5bcfb401d047e1712d1ee
Robert Grider <robert.grider@northwestern.edu>
2014-12-01 15:03:30 -0600

    Track changes in NetLogo repo

--- src/MatrixExtension.java
+++ src/MatrixExtension.java
@@ -2,7 +2,7 @@ package org.nlogo.extensions.matrix;
 
 import org.nlogo.core.Syntax;
 import org.nlogo.core.SyntaxJ;
-import org.nlogo.api.CompilerException;
+import org.nlogo.core.CompilerException;
 import org.nlogo.api.LogoException;
 import org.nlogo.api.ExtensionException;
 import org.nlogo.api.Argument;
